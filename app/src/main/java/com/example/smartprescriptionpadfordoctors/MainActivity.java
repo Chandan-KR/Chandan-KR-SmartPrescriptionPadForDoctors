8506cf570a0e954c940d7ec057ba3465bda48a2e
chandan k r <chandankr17032002@gmail.com>
2023-04-10 23:37:26 +0530

    commit 1.3

--- app/src/main/java/com/example/smartprescriptionpadfordoctors/MainActivity.java
+++ app/src/main/java/com/example/smartprescriptionpadfordoctors/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.smartprescriptionpadfordoctors;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.smartprescriptionpadfordoctors.form.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(3*1000);
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }catch (Exception e){
+
+                   e.printStackTrace();
+            }
+            super.run();
+
     }
-}
+};thread.start();
---
+++ app/src/main/java/com/example/smartprescriptionpadfordoctors/form/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.smartprescriptionpadfordoctors.form;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartprescriptionpadfordoctors.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
