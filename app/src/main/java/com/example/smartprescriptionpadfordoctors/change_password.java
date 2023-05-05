fe983bcd4f8edb050d154051c2c2188fb7a0ed91
chandan k r <chandankr17032002@gmail.com>
2023-05-05 22:54:32 +0530

    commit 1.8

--- app/src/main/java/com/example/smartprescriptionpadfordoctors/change_password.java
+++ app/src/main/java/com/example/smartprescriptionpadfordoctors/change_password.java
@@ -1,64 +1,70 @@
 package com.example.smartprescriptionpadfordoctors;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class change_password extends AppCompatActivity {
-    EditText firstPassword;
-    EditText confirmPassword;
-    Button set_password;
-    FirebaseAuth mAuth;
+    EditText passwordEditText;
+    EditText confirmPasswordEditText;
+    Button updatePasswordButton;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
 
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
+// Get references to the views
+        passwordEditText = findViewById(R.id.firstPassword);
+        confirmPasswordEditText = findViewById(R.id.confirmPassword);
+        updatePasswordButton = findViewById(R.id.set_password);
 
-        assert user != null;
-        if (user.isEmailVerified()) {
-            // get new password from user input
-            firstPassword = findViewById(R.id.firstPassword);
-            confirmPassword = findViewById(R.id.confirmPassword);
-            set_password = findViewById(R.id.set_password);
-            String newPassword = String.valueOf(firstPassword.getText());
-            String confirmPass = String.valueOf(confirmPassword.getText());
+// Set an onclick listener for the update password button
+        updatePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the new password and confirm password
+                String password = passwordEditText.getText().toString();
+                String confirmPassword = confirmPasswordEditText.getText().toString();
 
-            set_password.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (!newPassword.equals(confirmPass)) {
-                        Toast.makeText(change_password.this, "password not matched", Toast.LENGTH_SHORT).show();
-                    } else {
-                        // update password
-                        user.updatePassword(newPassword).addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                // Password updated successfully
-                                Toast.makeText(change_password.this, "Password updated successfully", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(change_password.this, signIn.class);
-                                startActivity(intent);
-                            } else {
-                                // Failed to update password
-                                Toast.makeText(change_password.this, "Failed to update password", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
+                // Check if the password fields are empty
+                if (password.isEmpty() || confirmPassword.isEmpty()) {
+                    Toast.makeText(change_password.this, "Please enter a new password.", Toast.LENGTH_SHORT).show();
                 }
-            });
-        } else {
-            // Email is not verified
-            Toast.makeText(change_password.this, "Email is not verified", Toast.LENGTH_SHORT).show();
-        }
+                // Check if the passwords match
+                else if (!password.equals(confirmPassword)) {
+                    Toast.makeText(change_password.this, "Passwords don't match. Please try again.", Toast.LENGTH_SHORT).show();
+                }
+                // Update the password
+                else {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    assert user != null;
+                    user.updatePassword(password).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(change_password.this, "Password updated successfully.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(change_password.this, "Password update failed. Please try again.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
