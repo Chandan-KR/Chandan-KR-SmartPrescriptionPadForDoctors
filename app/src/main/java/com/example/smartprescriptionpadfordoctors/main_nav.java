eba9163e50721ba8d41c8e1e0b3d4ed5d82c3d57
chandan k r <chandankr17032002@gmail.com>
2023-06-01 21:37:13 +0530

    commit 2.1

--- app/src/main/java/com/example/smartprescriptionpadfordoctors/main_nav.java
+++ app/src/main/java/com/example/smartprescriptionpadfordoctors/main_nav.java
@@ -1,6 +1,8 @@
 package com.example.smartprescriptionpadfordoctors;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,12 +21,13 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.smartprescriptionpadfordoctors.databinding.ActivityMainNavBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class main_nav extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainNavBinding binding;
-
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +49,7 @@ public class main_nav extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_history, R.id.nav_profile)
+                R.id.nav_home, R.id.nav_history, R.id.nav_profile,R.id.nav_logout)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_nav);
@@ -76,8 +80,12 @@ public class main_nav extends AppCompatActivity {
                 return true;
 
             case R.id.nav_logout:
-                
+                    logout();
                 return true;
+
+            case R.id.nav_logout1:
+                logout();
+
         }
         return super.onOptionsItemSelected(item);
     }
@@ -89,5 +97,30 @@ public class main_nav extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_nav);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    public void logout()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to log out?");
+        Intent intent = new Intent(this,signIn.class);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // User clicked "Yes" button, perform sign out
+                firebaseAuth.signOut();
+                // Add any additional logic you want to perform after signing out
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // User clicked "No" button, dismiss the dialog
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
